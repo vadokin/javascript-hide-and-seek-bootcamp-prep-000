532aca31b72d3e84c9c3c6c49486caaf18b8bd01
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-17 13:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function deepestChild() {
 function increaseRankBy(n) {
   let lis = document.querySelectorAll('.ranked-list li')
   for (let i = 0, len = lis.length; i < len; i++) {
-    lis[i].innerHTML = parseInt(lis[i].innerHTML) + 1 
+    lis[i].innerHTML = parseInt(lis[i].innerHTML, 10) + 1 
   }
 }
